f9db747ab61feb59e3f78d996b880521059db6cc
Raki <quangdatltw@gmail.com>
2025-06-13 23:42:49 +0700

    init project structure

---
+++ src/main/java/cloud/arciel/log/ArcielLogApplication.java
@@ -0,0 +1,13 @@
+package cloud.arciel.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcielLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcielLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/arciel/log/ArcielLogApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.arciel.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcielLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
